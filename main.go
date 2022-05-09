168d7663b52f87c0bcbf0f70f1c97429b9b412f2
yuan <hhhxoly@gamil.com>
2022-05-09 09:19:17 +0800

    优雅的重启 调用endless库实现 更新服务器无需强制停止

--- main.go
+++ main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/fvbock/endless"
 	"github.com/yuan0408/go-gin-example/pkg/setting"
 	"github.com/yuan0408/go-gin-example/routers"
-	"net/http"
+	"log"
+	"syscall"
 )
 
 func main() {
-	router := routers.InitRouter()
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	//给进程发送SIGTERM信号实现优雅的重启
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endpoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	server := endless.NewServer(endpoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
-	s.ListenAndServe()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
